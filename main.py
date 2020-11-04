e3ab21c956e90a8e662fe952e1f9477bc1318409
poulomi-g <poulomi@ualberta.ca>
2020-11-04 21:54:08 +0530

    Main menu added

--- main.py
+++ main.py
@@ -25,11 +25,49 @@ def main():
 
         path = sys.argv[1]
         conn = sqlite3.connect(path)
-
+        os.system('clear')
         print("Connection successful!")
 
     except:
-        print("Error, exiting...")
+        print("Input error, exiting...")
+
+    db = conn.cursor()
+
+    # Menu action will run every time there input error
+    while True:
+        print("********************************")
+        print()
+        print("1. Register")
+        print("2. Login")
+        print("3. Exit")
+        print()
+        print("********************************")
+        print()
+        action = input("Choose from one of the above: ")
+
+        try:
+            action = int(action)
+        except:
+            os.system('clear')
+            print("Input could not be casted to integer. Please enter digit")
+            continue
+
+        # If input integer:
+
+        if action == 1:
+            register(conn, db)
+
+        elif action == 2:
+            login(conn, db)
+
+        elif action == 3:
+            print("Exiting...")
+            exit(0)
+
+        else:
+            os.system('clear')
+            print("Input not available!")
+            continue
 
 
 if __name__ == '__main__':
@@ -37,5 +75,6 @@ if __name__ == '__main__':
         main()
 
     except KeyboardInterrupt:
+        print()
         print("Exiting...")
         exit(0)
